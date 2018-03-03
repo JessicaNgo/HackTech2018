9a922f2a244df955c3daba435f86d469878b720e
JessicaNgo <jessica.ngo@live.com>
2018-03-03 11:57:18 -0800

    base DRF with token auth

---
+++ server/api/urls.py
@@ -0,0 +1,10 @@
+# file: api/urls.py
+
+from django.conf.urls import url
+from rest_framework.authtoken import views as drf_views
+
+app_name='api'
+
+urlpatterns = [
+    url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
+]
